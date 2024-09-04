a59dbdfe7fc01018c07a5e64cf3718e33056d6f3
AsifZaman777 <eramasif16@gmail.com>
2024-09-05 01:27:02 +0600

    pastPaper section init

--- arc-client/src/App.jsx
+++ arc-client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route,Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Admin from "./pages/adminPage/Admin";
 import "./App.css";
+import PastPapers from "./components/pastPapers/PastPapers";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   <Routes>
     <Route path="/" element={<Landing />} />
     <Route path="/dashboard/*" element={<Admin />} />
+    <Route path="/pastpapers/*" element={<PastPapers/>} />
   </Routes>
   )
 }
---
+++ arc-client/src/components/pastPapers/PastPapers.jsx
@@ -0,0 +1,11 @@
+
+
+const PastPapers = () => {
+    return (
+        <div>
+            <h2 className='text-center text-orange-300'>Past Papers</h2>
+        </div>
+    );
+};
+
+export default PastPapers;
