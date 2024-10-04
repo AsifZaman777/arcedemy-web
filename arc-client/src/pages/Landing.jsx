af77f19e4e990cd1bf2cfb276e8f40156ec47b8c
AsifZaman777 <eramasif16@gmail.com>
2024-10-05 03:55:39 +0600

    navbar fixed

--- arc-client/src/pages/Landing.jsx
+++ arc-client/src/pages/Landing.jsx
@@ -1,7 +1,6 @@
 import Home from "../components/Home";
 import About from "../components/About";
 import Service from "../components/Service";
-import OurTeam from "../components/OurTeam";
 import Course from "../components/Course";
 import FAQSection from "../components/FAQ"; // Corrected import path
 import Contact from "../components/Contact";
@@ -18,7 +17,7 @@ const Landing = () => {
             <Service />
             <Course />
             <FAQSection />
-            <OurTeam />
+            {/* <OurTeam /> */}
             <Contact />
             <Footer />
         </div>
